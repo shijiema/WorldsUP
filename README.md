47bcfb8cc666f78717c30874b8e1b537012bfc8a
Andrew Ma <andrew.ma@ge.com>
2018-01-07 18:59:52 -0800

    backup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# WorldsUp
+# WorldsUP
 Simple but useful location based app that allows users to post real world events that lives only short period of time.
--- app/src/main/java/io/worldsup/worldsup/MapsActivity.java
+++ app/src/main/java/io/worldsup/worldsup/MapsActivity.java
@@ -1,18 +1,180 @@
 package io.worldsup.worldsup;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
+    private static final String TAG = MapsActivity.class.getSimpleName();
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 1000;
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+    private Location mCurrentLocation;
+    // A request object to store parameters for requests to the FusedLocationProviderApi.
+    private LocationRequest mLocationUpdateRequest;
+    private static final int PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 2;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private GoogleMap mMap;
+    //for google service access
+    private GoogleApiClient mGoogleApiClient;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private boolean mLocationPermissionGranted;
+    private LatLng defaultLocationSydney = new LatLng(-34, 151);
+
+    /**
+     * Sets up the location request.
+     */
+    private void createLocationRequest() {
+        mLocationUpdateRequest = new LocationRequest();
+
+        /*
+         * Sets the desired interval for active location updates. This interval is
+         * inexact. You may not receive updates at all if no location sources are available, or
+         * you may receive them slower than requested. You may also receive updates faster than
+         * requested if other applications are requesting location at a faster interval.
+         */
+        mLocationUpdateRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        /*
+         * Sets the fastest rate for active location updates. This interval is exact, and your
+         * application will never receive updates faster than this value.
+         */
+        mLocationUpdateRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationUpdateRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    /**
+     * build google api client location and place service. not using place service for now
+     */
+    private synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,
+                        this /* OnConnectionFailedListener */)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                //.addOnConnectionFailedListener(this)
+                //.addApi(Places.GEO_DATA_API)
+                //.addApi(Places.PLACE_DETECTION_API)
+                .build();
+        //createLocationRequest();
+        mGoogleApiClient.connect();
+    }
+    /**
+     * Gets the current location of the device and starts the location update notifications.
+     */
+    private void getDeviceLocation() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        //check if it has the permission
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            //explicitly request permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+        }
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         * Also request regular updates about the device location.
+         */
+        if (mLocationPermissionGranted) {
+//            mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+//            if(mCurrentLocation!=null)
+//                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+//                        mLocationUpdateRequest, this);
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+//            if (
+//                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+//                            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+//                    ) {
+//                // TODO: Consider calling
+//                //    ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                //                                          int[] grantResults)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//                return;
+//            }
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                mCurrentLocation = location;
+                            }
+                        }
+                    });
+        }
+        else
+            Toast.makeText(this, "location permission is not granted!", Toast.LENGTH_LONG).show();
+    }
+    /**
+     * Handles the result of the request for location permissions.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        //mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+       // updateLocationUI();
+    }
+    /**
+     * Updates the map's UI settings based on whether the user has granted location permission.
+     */
+    @SuppressWarnings("MissingPermission")
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+
+        if (mLocationPermissionGranted) {
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        } else {
+            mMap.setMyLocationEnabled(false);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            mCurrentLocation = null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +184,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        buildGoogleApiClient();
     }
 
 
@@ -37,10 +200,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //getDeviceLocation();
+        //gotoCurrentLocation();
     }
-}
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "connection failed!", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Toast.makeText(this, "connected!", Toast.LENGTH_LONG).show();
+        getDeviceLocation();
+        gotoCurrentLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        //gotoCurrentLocation();
+    }
+
+    private void gotoCurrentLocation() {
+        LatLng curGps = null;
+        MarkerOptions marker = null;
+
+        if (mCurrentLocation == null) {
+                Toast.makeText(this, "use default location!", Toast.LENGTH_SHORT).show();
+                curGps = defaultLocationSydney;
+                marker = new MarkerOptions().position(curGps).title("Seydny");
+            } else {
+
+                curGps = new LatLng(
+                        mCurrentLocation.getLatitude(),
+                        mCurrentLocation.getLongitude()
+                );
+                marker =new MarkerOptions().position(curGps).title(mCurrentLocation.toString());
+                CameraUpdate update = CameraUpdateFactory.newLatLngZoom(
+                        curGps, 10
+                );
+                //mMap.animateCamera(update);
+            }
+            mMap.addMarker(marker);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(curGps));
+
+        }
+
+    }
